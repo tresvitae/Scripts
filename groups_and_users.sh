86649847639309422cf48355ebe0d1d02705f783
Patryk Futa <patryk.futa@gmail.com>
2021-03-16 20:02:12 +0100

    Creating new user

--- groups_and_users.sh
+++ groups_and_users.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-echo "Enter new group name:"
+echo -n "Enter new group name: \n"
 read GROUP_NAME
 while grep -q $GROUP_NAME /etc/group
   do
-      echo "This group already exists. Please type again:"
-      read GROUP_NAME
+    echo "This group already exists. Please type again:"
+    read GROUP_NAME
   done
 groupadd $GROUP_NAME
 echo "Group created successfuly!"
+
+echo -n "Enter a username name for the new user: \n"
+read USER_NAME
+
+getent passwd $username >> /dev/null
+
+while [ $? -eq 0 ]
+  do
+    echo -n "User aleady exists!\nPlease type another one: "
+    read USER_NAME
+    getent passwd $USER_NAME >> /dev/null
+  done
+
+
