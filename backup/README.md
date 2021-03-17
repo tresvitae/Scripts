7eb4c3d6e17ac5def3eb1e272d124e49038a373e
Patryk Futa <patryk.futa@gmail.com>
2021-03-17 22:03:57 +0100

    Multiply users option

--- backup/README.md
+++ backup/README.md
@@ -1,7 +1,7 @@
 # Simple backup
 
 `$ sudo ./users-home-dir.sh`
-or `$ sudo ./users-home-dir.sh USER_NAME`
+or `$ sudo ./users-home-dir.sh 1_OR_MORE_USER_NAMES`
 
 ### Case Scenario
 Creating a simple backup of user's home directory.
@@ -14,6 +14,6 @@ Create a bash script to perform user management tasks as outlined below:
     - Two functions to report a number of directories and files
     - Add conditional statements to implement a sanity check to compare the number of files and directories within a source directory before and after the backup command
     - Select a system user for backup. By default the scrip will backup a current user's home directory
-
+    - Set ability to backup all directories supplied to the script on a command line upon its execution
 
 Source: https://linuxconfig.org/bash-scripting-tutorial-for-beginners
--- backup/users-home-dir.sh
+++ backup/users-home-dir.sh
@@ -1,62 +1,63 @@
 #!/bin/bash
 
-# -z to check whether positional parameter $1 contains any value. Return true if length of string is zero.
-if [ -z $1 ]; then
-    user=$(whoami)
-else
-# -d to check if user's home dir exists.
-    if [ ! -d "/home/$1" ]; then
-        echo "Requested $1 user home directory doesn't exist."
-        exit 1
+function backup {
+    # -z to check whether positional parameter $1 contains any value. Return true if length of string is zero.
+    if [ -z $1 ]; then
+        user=$(whoami)
+    else
+    # -d to check if user's home dir exists.
+        if [ ! -d "/home/$1" ]; then
+            echo "Requested $1 user home directory doesn't exist."
+            exit 1
+        fi
+        user=$1
     fi
-    user=$1
-fi
 
-input=/home/$user
-output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+    input=/home/$user
+    output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-# Reports a total number of files for a given directory.
-function total_files {
-    find $1 -type f | wc -l
+    # Reports a total number of files for a given directory.
+    function total_files {
+        find $1 -type f | wc -l
+    }
+
+    function total_dir {
+        find $1 -type d | wc -l
+    }
+
+    function total_arch_files {
+        tar -tzf $1 | grep -v /$ | wc -l
+    }
+
+    function total_arch_dir {
+        tar -tzf $1 | grep /$ | wc -l
+    }
+
+    tar -czf $output $input 2> /dev/null
+
+    src_files=$( total_files $input )
+    arch_files=$( total_arch_files $output )
+
+    src_dir=$( total_dir $input )
+    arch_dir=$( total_arch_dir $output )
+
+    # Output message.
+    echo "########## $user ##########"
+    echo "Files to be included: $src_files"
+    echo "Directories to be included: $src_dir"
+    echo "Files archived: $arch_files"
+    echo "Directories archived: $arch_dir"
+
+    # Sanity check of number of files and dirs within user's home dir before and after the backup command.
+    if [ $src_files -eq $arch_files ]; then
+        echo "Backup of $input completed! Details: "
+        ls -l $output
+    else
+        echo "Backup of $input failed!"
+    fi
 }
 
-function total_dir {
-    find $1 -type d | wc -l
-}
-
-function total_arch_files {
-    tar -tzf $1 | grep -v /$ | wc -l
-}
-
-function total_arch_dir {
-    tar -tzf $1 | grep /$ | wc -l
-}
-
-tar -czf $output $input 2> /dev/null
-
-src_files=$( total_files $input )
-arch_files=$( total_arch_files $output )
-
-src_dir=$( total_dir $input )
-arch_dir=$( total_arch_dir $output )
-
-echo "Files to be included: $src_files"
-echo "Directories to be included: $src_dir"
-echo "Files archived: $arch_files"
-echo "Directories archived: $arch_dir"
-
-# Output message.
-echo -n "Files to be included: "
-total_files
-
-echo -n "Directories to be included: "
-total_dir
-
-
-# Sanity check of number of files and dirs within user's home dir before and after the backup command.
-if [ $src_files -eq $arch_files ]; then
-    echo "Backup of $input completed! Details: "
-    ls -l $output
-else
-    echo "Backup of $input failed!"
-fi
+# Multiply backup for all supplied as an argument.
+for directory in $*; do
+    backup $directory
+done;
