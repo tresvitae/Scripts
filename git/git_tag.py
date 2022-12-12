e779f3f93cbc3f235e3fc9f1018e3df32f16910b
tresvitae <patryk.futa@gmail.com>
2022-12-12 13:00:45 +0100

    Call of the shell should be replaced with GitPython module usage

--- git/git_tag.py
+++ git/git_tag.py
@@ -1,14 +1,21 @@
-import subprocess
 import argparse
+import git
+from git import Repo
 
+#Parse arguments using argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('tag_name', help='The name of the tag to create.')
-parser.add_argument('commit', help='The commit to tag.', default='Fast fix.')
+parser.add_argument('tag_name', help='The name/number of the tag.')
+parser.add_argument('commit_id', help='The commit to tag.', default='HEAD')
 args = parser.parse_args()
 
-tag = args.tag_name
-commit = args.commit
-command = f'git tag -a {tag} {commit} -m "{tag}"'
-output = subprocess.check_output(command, shell=True).decode('utf-8')
-subprocess.call(command, shell=True)
-subprocess.call('git push --tags', shell=True)
+#Init the repo
+repo = Repo('PWD')
+
+#Create the tag and push it to the remote repository
+try:
+    tag_obj = repo.create_tag(args.tag_name, args.commit_id)
+    repo.remotes.origin.push(tag_obj)
+except Exception as error:
+    print(f'Error: {error}')
+
+
